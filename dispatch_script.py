844ff707f0982accf6636f65cc2ce40091a48929
ghopkins3 <ghopkins3@murraystate.edu>
2025-11-29 22:32:00 -0600

    added checkcode encoding to metadata and inserting into print data

--- dispatch_script.py
+++ dispatch_script.py
@@ -2,35 +2,7 @@ import os, random, base64;
 
 # input n amount of orders
 # output n amount of order data and print data pairs
-"""
-<Host2KiSoft messageID="151">
-<OrderData orderNumber="KNAPP_NO_LBA" sheetNumber="1">
-<OrderType>15</OrderType>
-<LoadUnitCode>01005957</LoadUnitCode>
-<LoadCarrier>NE1</LoadCarrier>
-<DispatchRampNumbers>
-<DispatchRampNumber>116</DispatchRampNumber>
-</DispatchRampNumbers>
-<ControlParameters>
-<ControlParameter>1</ControlParameter>
-</ControlParameters>
-</OrderData>
-</Host2KiSoft>
 
-<Host2KiSoft messageID="150">
-<PrintDataMessage orderNumber="KNAPP_NO_LBA">
-<DispatchRampNumbers>
-<DispatchRampNumber>116</DispatchRampNumber>
-</DispatchRampNumbers>
-<ControlParameters>
-<ControlParameter>9003</ControlParameter>
-</ControlParameters>
-<CheckCode>11110000642114410302</CheckCode>
-</PrintDataMessage>
-</Host2KiSoft>
-"""
-
-# def input_dispatch_order_params():
 orderAmount = 0;
 orderNumber = "";
 starting_load_unit_code = 0;
@@ -48,37 +20,6 @@ default_checkcode = """^XA
 ^FO0,1280^GB812,0,6^FS
 ^FO80,1370^BY4^BCN,200,Y,N^FD>511110000072114410302^FS
 ^XZ"""
-
-def encode_checkcode():
-    encoded_checkcode = base64.b64encode(b'^XA^FO0,20,2^A0N,200,200^FB812,1,1,C^FD\&^FS^FO30,200^GB750,400,10,B,5^FS^FO0,280,2^A0N,200,200^FB812,1,1,C^FDA#CWM\&^FS^FO50,650,2^A0N,40,40^FB812,1,1,L^FDGONZ-210162^FS^FO50,700,2^A0N,30,30^FB750,20,7,L^FD99995000\&^FS^FO20,1250,2^A0N,30,30^FB812,1,1,L^FD09.10.25 14:34:44\&^FS^FO0,1280^GB812,0,6^FS^FO80,1370^BY4^BCN,200,Y,N^FD>511110000072114410302^FS^XZ');
-
-    encoded_checkcode = str(encoded_checkcode).replace("'", "").replace("b", "");
-    print(encoded_checkcode);
-
-# randomize checkcode
-# change last 3 character randomly
-# generate metadata
-# insert both into printdata
-
-starting_checkcode = "511110000072114410302";
-
-
-print(starting_checkcode);
-print(len(starting_checkcode));
-random_five_digit_num = random.randrange(10**5);
-    
-print(random_five_digit_num);
-split_checkcode_at = 16;
-split_checkcode = starting_checkcode[:split_checkcode_at];
-print(split_checkcode);
-new_checkcode = split_checkcode + str(random_five_digit_num);
-print(new_checkcode);
-print(len(new_checkcode));
-metadata_to_decode = """XlhBCl5GTzAsMjAsMl5BME4sMjAwLDIwMF5GQjgxMiwxLDEsQ15GRFwmXkZTCl5GTzMwLDIwMF5HQjc1MCw0MDAsMT
-AsQiw1XkZTCl5GTzAsMjgwLDJeQTBOLDIwMCwyMDBeRkI4MTIsMSwxLENeRkRBI0NXTVwmXkZTCl5GTzUwLDY1MCwyXkEwTiw0MCw0MF5GQjgxMiwx
-LDEsTF5GREdPTlotMjEwMTYyXkZTCl5GTzUwLDcwMCwyXkEwTiwzMCwzMF5GQjc1MCwyMCw3LExeRkQ5OTk5NTAwMFwmXkZTCl5GTzIwLDEyNTAsMl
-5BME4sMzAsMzBeRkI4MTIsMSwxLExeRkQwOS4xMC4yNSAxNDozNDo0NFwmXkZTCl5GTzAsMTI4MF5HQjgxMiwwLDZeRlMKXkZPODAsMTM3MF5CWTRe
-QkNOLDIwMCxZLE5eRkQ+NTExMTEwMDAwMDcyMTE0NDEwMzAyXkZTCl5YWg=="""
 
 order_data_template = [
     '<Host2KiSoft messageID="151">',
@@ -105,13 +46,22 @@ print_data_template = [
     '<ControlParameter>11</ControlParameter>',
     '</ControlParameters>',
     '<CheckCode>{checkcode}</CheckCode>',
-    '<MetaData>metadata</MetaData>',
+    '<MetaData>{metadata}</MetaData>',
     '</PrintDataMessage>',
     '</Host2KiSoft>',   
 ]
 
 generated_order_data = [];
 generated_print_data = [];
+
+def encode_checkcode(checkcode):
+    checkcode_bytes = checkcode.encode("ascii");
+
+    base64_bytes = base64.b64encode(checkcode_bytes);
+    print(base64_bytes);
+    metadata = base64_bytes.decode("ascii");
+    print(metadata);
+    return metadata;
 
 while(os.path.exists(filepath)): 
     count += 1;
@@ -164,6 +114,7 @@ startOrderNumber = orderNumber;
 starting_dispatch_ramp = 114;
 seq_dispatch_ramp = starting_dispatch_ramp;
 
+starting_checkcode = "11110000072114410302"
 
 load_unit_code = starting_load_unit_code;
 
@@ -175,11 +126,22 @@ for i in range(orderAmount):
     random_five_digit_num = random.randrange(10**5);
     
     print(random_five_digit_num);
-    split_checkcode_at = 16;
+    split_checkcode_at = 15;
     split_checkcode = starting_checkcode[:split_checkcode_at];
     print(split_checkcode);
     new_checkcode = split_checkcode + str(random_five_digit_num);
+    
     print(new_checkcode);
+
+    split_default_checkcode_at = 339;
+    split_default_checkcode = default_checkcode[:split_default_checkcode_at];
+    print(default_checkcode);
+    print(split_default_checkcode);
+    new_checkcode_to_encode = split_default_checkcode + "5" + new_checkcode + "^FS" + "\n" + "^XZ";
+    print(new_checkcode_to_encode);
+
+    encode_checkcode(new_checkcode_to_encode);
+    print(encode_checkcode(new_checkcode_to_encode));
 
     if(i % 14 == 0):
         seq_dispatch_ramp = starting_dispatch_ramp;
@@ -198,7 +160,7 @@ for i in range(orderAmount):
 
     new_order_data = [line.format(orderNumber = orderNumber, load_unit_code = load_unit_code, dispatch_ramp = seq_dispatch_ramp)
                         for line in order_data_template];
-    new_print_data = [line.format(orderNumber=orderNumber, dispatch_ramp = seq_dispatch_ramp, checkcode = new_checkcode)
+    new_print_data = [line.format(orderNumber=orderNumber, dispatch_ramp = seq_dispatch_ramp, checkcode = new_checkcode, metadata = encode_checkcode(new_checkcode_to_encode))
                         for line in print_data_template];
 
 
@@ -221,10 +183,3 @@ def write_dispatch_orders(filepath, tuple):
         print(f.read());
 
 write_dispatch_orders(filepath, joined_order_data);
-
-def print_tuple(tuple):
-    ele_count = 0;
-    for n in tuple:
-        ele_count += 1;
-        print(n);
-        print("count: ", ele_count);
